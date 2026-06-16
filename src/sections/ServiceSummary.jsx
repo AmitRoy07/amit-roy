9260e3bedfb72477367d4ff7c2787a422eea0d45
Amit <amit.roy@sentientgeeks.com>
2026-06-16 20:11:18 +0530

    feat: update service summary to reflect correct tools and technologies

--- src/sections/ServiceSummary.jsx
+++ src/sections/ServiceSummary.jsx
@@ -75,12 +75,12 @@ const ServiceSummary = () => {
         className="flex items-center justify-center gap-3 -translate-x-48"
       >
         <p>
-          <SummaryWord icon="lucide:figma">Figma</SummaryWord>
+          <SummaryWord icon="lucide:figma">Wordpress</SummaryWord>
         </p>
         <div className="w-10 h-1 md:w-32 bg-gold" />
         <p>
           <SummaryWord icon="lucide:pen-tool" className="italic">
-            UI/UX
+            UI/UX - Figma
           </SummaryWord>
         </p>
         <div className="w-10 h-1 md:w-32 bg-gold" />
